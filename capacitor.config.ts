ebc9512ebb23099efc58909eb109d3d48e333759
Subham Saha <subham@Subhams-MacBook-Pro.local>
2023-12-08 04:44:58 +0530

    done with splash screen

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -6,6 +6,21 @@ const config: CapacitorConfig = {
   webDir: 'dist',
   server: {
     androidScheme: 'https'
+  },
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 2000,
+      launchAutoHide: true,
+      launchFadeOutDuration: 1000,
+      backgroundColor: "#ffffffff",
+      androidSplashResourceName: "splash",
+      androidScaleType: "CENTER_CROP",
+      showSpinner: false,
+      splashFullScreen: true,
+      splashImmersive: true,
+      layoutName: "launch_screen",
+      useDialog: true,
+    },
   }
 };
 
